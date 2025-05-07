edd441f6c22a540fe0b3e3de8c26afc16868d7aa
박시후 <hoo06240624@gmail.com>
2025-05-07 20:21:46 +0900

    긴 글

--- test1.py
+++ test1.py
@@ -5,8 +5,8 @@ tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
 with col1 :
     st.title("here is column1 title")
     with tab1 :
-        st.write('hello')
-    with tab1 :
+        st.write('존재하는 이유 그런건 아무래도 좋으니 그리 즐겁지도 괴롭지도 않은 바람아 불어라 달을 찾는 이유 예쁜 건 언제봐도 좋으니 나는 세계의 시계를 부수고 너에게 닿는다')
+    with tab2 :
         st.write("hi")
 
 with col2 :
