d678c15ff5e48f7b337cd9845406333b60a72124
박시후 <hoo06240624@gmail.com>
2025-05-07 20:13:24 +0900

    변수 건드리기

--- test1.py
+++ test1.py
@@ -3,6 +3,10 @@ import streamlit as st
 col1, col2 = st.columns([2,3])
 with col1 :
     st.title("here is column1 title")
+
 with col2 :
     st.title("here is column2 title")
     st.checkbox("this is checkbox1 in col2 ")
+
+col1.subheader("I am column1 subheader")
+col2.checkbox("this is checkbox2 in col2")
