f16aa78ee00ba73ca00462672bb27eab7a66d447
박시후 <hoo06240624@gmail.com>
2025-05-07 20:19:18 +0900

    순서 바꾸기

--- test1.py
+++ test1.py
@@ -4,9 +4,9 @@ col1, col2 = st.columns([2,3])
 tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
 with col1 :
     st.title("here is column1 title")
-    with tab1 :
-        st.write('hello')
     with tab2 :
+        st.write('hello')
+    with tab1 :
         st.write("hi")
 
 with col2 :
