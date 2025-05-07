2a4ef68f24568f6bf3c3812a63c8e6f59d990f85
박시후 <hoo06240624@gmail.com>
2025-05-07 20:19:58 +0900

    둘 다 탭1

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ col1, col2 = st.columns([2,3])
 tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
 with col1 :
     st.title("here is column1 title")
-    with tab2 :
+    with tab1 :
         st.write('hello')
     with tab1 :
         st.write("hi")
